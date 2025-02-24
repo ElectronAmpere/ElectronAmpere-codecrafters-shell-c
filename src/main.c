0e4fbfd8ca4a7c531cad32a96bf9e6511e0dea88
ElectronAmpere <learningthiruvalluvar@gmail.com>
2025-02-24 21:38:33 +0530

    Added code for REPL: Trial

--- src/main.c
+++ src/main.c
@@ -6,17 +6,21 @@
 #define GET_ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
-  printf("$ ");
+  while(1)
+  {
+    // Flush after every printf
+    setbuf(stdout, NULL);
 
-  // Wait for user input
-  char input[MAX_INPUT_COMMAND_LENGTH] = {0};
-  fgets(input, GET_ARRAY_SIZE(input), stdin);
+    // Uncomment this block to pass the first stage
+    printf("$ ");
 
-  input[strlen(input) - 1] = '\0';
-  printf("%s: command not found\n", input);
+    // Wait for user input
+    char input[MAX_INPUT_COMMAND_LENGTH] = {0};
+    fgets(input, GET_ARRAY_SIZE(input), stdin);
+
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
   return 0;
 }
