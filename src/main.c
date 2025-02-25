2c299ce032dea11a27231ad15ac4da4fdbeb6667
ElectronAmpere <learningthiruvalluvar@gmail.com>
2025-02-25 21:27:55 +0530

    implemented type buitlin commands

--- src/main.c
+++ src/main.c
@@ -17,19 +17,40 @@ int main(int argc, char *argv[]) {
 
     // Wait for user input
     char input[MAX_INPUT_COMMAND_LENGTH] = {0};
-    char exit_string[] = "exit 0";
-    char echo_string[] = "echo";
+    char exit_cmd[] = "exit 0";
+    char echo_cmd[] = "echo";
+    char type_cmd[] = "type"
+
     fgets(input, GET_ARRAY_SIZE(input), stdin);
 
     input[strlen(input) - 1] = '\0';
-    if (strncmp(input, exit_string, strlen(exit_string)) == 0)
+    if (strncmp(input, exit_cmd, strlen(exit_cmd)) == 0)
     {
       return 0;
     }
-    else if (strncmp(input, echo_string, strlen(echo_string)) == 0)
+    else if (strncmp(input, echo_cmd, strlen(echo_cmd)) == 0)
     {
-      printf("%s\n", input + strlen(echo_string) + 1);
+      printf("%s\n", input + strlen(echo_cmd) + 1);
       continue;
+    }
+    else if (strncmp(input, type_cmd, strlen(type_cmd)) == 0)
+    {
+      if (strncmp((input + strlen(type_cmd) + 1), type_cmd, strlen(type_cmd)) == 0)
+      {
+        printf("%s is a shell builtin\n", input + strlen(type_cmd) + 1);
+      }
+      else if (strncmp((input + strlen(exit_cmd) + 1), exit_cmd, strlen(exit_cmd)) == 0)
+      {
+        printf("%s is a shell builtin\n", input + strlen(exit_cmd) + 1);
+      }
+      else if (strncmp((input + strlen(echo_cmd) + 1), echo_cmd, strlen(echo_cmd)) == 0)
+      {
+        printf("%s is a shell builtin\n", input + strlen(echo_cmd) + 1);
+      }
+      else
+      {
+        printf("%s: command not found\n", input);
+      }
     }
     else
     {
